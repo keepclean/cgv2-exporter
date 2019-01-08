be9f6c9f914a3d1a4354f7a3f177333b2e7453de
Rustam Iuldashev <rustam.iuldashev@protonmail.com>
2019-01-08 11:43:17 +0000

    Added test for parseKV func

---
+++ utils_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestParseKV(t *testing.T) {
+	// case #1 - test wrong string
+	s := "k1 v1 k2 v2"
+	_, _, err := parseKV(s)
+	if err == nil {
+		t.Errorf("ParseKV is broken in case #1")
+	}
+
+	// case #2 - test string with right format but wrong content
+	s = "k1 v1"
+	_, _, err = parseKV(s)
+	if err == nil {
+		t.Errorf("ParseKV is broken in case #2")
+	}
+
+	// case #3 - test correct string
+	s = "k1 2"
+	k, v, err := parseKV(s)
+	if err != nil || k != "k1" || v != 2 {
+		t.Errorf("ParseKV is broken in case #3")
+	}
+}
