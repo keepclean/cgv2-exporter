92356ed89a7d67ae041ce5815336b74064a8dd3d
Rustam Iuldashev <rustam.iuldashev@protonmail.com>
2019-01-11 22:00:05 +0000

    Added initilal test for TestCgroupIOMetrics func

--- io_test.go
+++ io_test.go
@@ -52,3 +52,41 @@ func TestParseIOStat(t *testing.T) {
 		t.Errorf("Problems with parsing io.stat file: got %f; wanted: %f", v, targetValue)
 	}
 }
+
+func TestCgroupIOMetrics(t *testing.T) {
+	cgDir = "./"
+	service := "s"
+
+	err := os.Mkdir(service, 0755)
+	if err != nil {
+		t.Error(err)
+	}
+
+	controllerFile := "io.stat"
+	f, err := os.Create(fmt.Sprint(service, "/", controllerFile))
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(service)
+
+	_, err = f.WriteString("8:0 rbytes=290260631552 wbytes=0 rios=6740371 wios=354068")
+	if err != nil {
+		t.Error(err)
+	}
+
+	devices = map[string]string{
+		"8:0": "sda",
+	}
+
+	// TODO Make test a bit smarter
+	cgroupIOMetrics(service, false)
+	if _, err := ioRbytes.GetMetricWithLabelValues(service, "sda"); err != nil {
+		t.Error("Something goes wrong... ", err)
+	}
+
+	cgroupIOMetrics(service, true)
+	if _, err := ioRios.GetMetricWithLabelValues(service, "/dev/sda"); err != nil {
+		t.Error("Something goes wrong... ", err)
+	}
+
+}
